955c8e775d24a606dcb921449e0824b9b03cbb51
Jörg Breitbart <jerch@rockborn.de>
2026-02-04 23:24:10 +0100

    license file

--- src/base64/Base64Decoder.wasm.ts
+++ src/base64/Base64Decoder.wasm.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 The xterm.js authors. All rights reserved.
+ * Copyright (c) 2023, 2026 The xterm.js authors. All rights reserved.
  * @license MIT
  */
 import { InWasm, OutputMode, OutputType } from 'inwasm-runtime';
